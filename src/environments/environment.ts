2ee2aab5957e046b735ec2a8d757453e64344a25
Nicole <petovellon@gmail.com>
2024-04-10 15:28:20 +0200

    Setup env placeholders

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: import.meta.env['NG_API_URL'],
-  apiKey: import.meta.env['NG_API_KEY'],
+  apiUrl: '__API_URL__',
+  apiKey: '__API_KEY__',
 };
